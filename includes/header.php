0fa6565742211b62b7d6d28d52f765922582b771
Elwilya <69197349+Elwilya@users.noreply.github.com>
2021-01-21 19:15:43 +0100

    Fin slide 18

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,14 @@
 <?php
+session_start();
+
 require_once 'includes/Fonctions.php';
 require_once 'includes/Variables.php';
+
+if (isset($_POST['username'])) {
+    $_SESSION['username'] = $_POST['username'];
+}
+var_dump($_SESSION);
+
 ?>
 
 <!DOCTYPE html>
@@ -12,5 +20,36 @@ require_once 'includes/Variables.php';
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
+<!--Fontawesome CDN-->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="index.php">Les beaux bonnets</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="index.php">Accueil</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="list.php">Liste</a>
+            </li>
+            <?php if (isset($_SESSION['username'])) { ?>
+                <li class="nav-item">
+                    <a class="nav-link" href="login.php"><?= $_SESSION['username']; ?></a>
+                </li>
+            <?php } else { ?>
+            <li class="nav-item">
+                <a class="nav-link" href="login.php">Connexion</a>
+            </li>
+                <?php } ?>
+            
+        </ul>
+    </div>
+</nav>
---
+++ login.php
@@ -0,0 +1,34 @@
+<?php
+include "includes/header.php";
+include "includes/footer.php";
+?>
+
+<div class="container">
+	<div class="d-flex justify-content-center h-100">
+		<div class="card">
+			<div class="card-header">
+				<h3>Connexion</h3>
+			</div>
+			<div class="card-body">
+				<form method="POST" action="">
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-user"></i></span>
+						</div>
+						<input type="text" class="form-control" name="username" placeholder="username">
+						
+					</div>
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-key"></i></span>
+						</div>
+						<input type="password" class="form-control" name="password" placeholder="password">
+					</div>
+					<div class="form-group">
+						<input type="submit" value="Se connecter" class="btn float-right login_btn">
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
